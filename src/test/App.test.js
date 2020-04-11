0df548d44454ec2f53bdeb4b59095ad3f305ba03
Jake Koperski <wijakoperski@gmail.com>
2020-04-11 00:07:10 -0500

    Fixed test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -4,6 +4,6 @@ import App from '../App/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/WE'RE WORKING ON IT. :\)/i);
   expect(linkElement).toBeInTheDocument();
 });
