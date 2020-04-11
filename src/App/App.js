8cf368edd50d1f537e1f363129838b487e0c776f
Jake Koperski <wijakoperski@gmail.com>
2020-04-10 23:46:55 -0500

    Project init

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='greeting'>WE'RE WORKING ON IT.</div>
+    </div>
+  );
+}
+
+export default App;
