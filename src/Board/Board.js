a733f85ab1b924df4e272f2c39889fcba8d11004
Jake Koperski <wijakoperski@gmail.com>
2020-04-12 22:05:03 -0500

    Converted Board to class component

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -2,52 +2,81 @@ import React from 'react';
 import './Board.css';
 import Cell from '../Cell/Cell';
 
-function createBoardModel(width, height, mineCount) {
-    const boardModel = [...Array(width).keys()].map(x =>
-        [...Array(height).keys()].map(y => ({
-            x,
-            y,
-            isMine: false,
-            mineCount: 0
-        }))
-    );
+export default class Board extends React.Component {
+    constructor(props) {
+        super(props);
 
-    // Create an array of length mineCount comprising random x and y values
-    // TODO: Enforce no duplicates
-    const set = new Set();
-    while (set.size < mineCount) {
-        set.add(JSON.stringify({
-            x: Math.round(Math.random() * (width - 1)),
-            y: Math.round(Math.random() * (height - 1))
-        }));
+        const mineCount = 100;
+        const width = 25;
+        const height = 25;
+
+        const boardModel = [...Array(width).keys()].map(x =>
+            [...Array(height).keys()].map(y => ({
+                x,
+                y,
+                isMine: false,
+                mineCount: 0,
+                isRevealed: false,
+                isFlagged: false
+            }))
+        );
+
+        // Create a set of length mineCount comprising unique random x and y values
+        const set = new Set();
+        while (set.size < mineCount) {
+            // Positions are stringified so the set can ignore duplicate positions
+            set.add(JSON.stringify({
+                x: Math.round(Math.random() * (width - 1)),
+                y: Math.round(Math.random() * (height - 1))
+            }));
+        }
+    
+        // Place the mines and increment the count of surrounding cells
+        [...set].map(JSON.parse).forEach(({ x, y }) => {
+            if (x > 0 && y > 0) if (!boardModel[x - 1][y - 1].isMine) boardModel[x - 1][y - 1].mineCount++;
+            if (x > 0) if (!boardModel[x - 1][y].isMine) boardModel[x - 1][y].mineCount++;
+            if (x > 0 && y < height - 1) if (!boardModel[x - 1][y + 1].isMine) boardModel[x - 1][y + 1].mineCount++;
+            if (y > 0) if (!boardModel[x][y - 1].isMine) boardModel[x][y - 1].mineCount++;
+            boardModel[x][y].isMine = true;
+            if (y < height - 1) if (!boardModel[x][y + 1].isMine) boardModel[x][y + 1].mineCount++;
+            if (x < width - 1 && y > 0) if (!boardModel[x + 1][y - 1].isMine) boardModel[x + 1][y - 1].mineCount++;
+            if (x < width - 1) if (!boardModel[x + 1][y].isMine) boardModel[x + 1][y].mineCount++;
+            if (x < width - 1 && y < height - 1) if (!boardModel[x + 1][y + 1].isMine) boardModel[x + 1][y + 1].mineCount++;
+        });
+
+        this.state = { matrix: boardModel };
     }
 
-    // Place the mines and increment the count of surrounding cells
-    [...set].map(JSON.parse).forEach(({ x, y }) => {
-        if (x > 0 && y > 0) if (!boardModel[x - 1][y - 1].isMine) boardModel[x - 1][y - 1].mineCount++;
-        if (x > 0) if (!boardModel[x - 1][y].isMine) boardModel[x - 1][y].mineCount++;
-        if (x > 0 && y < height - 1) if (!boardModel[x - 1][y + 1].isMine) boardModel[x - 1][y + 1].mineCount++;
-        if (y > 0) if (!boardModel[x][y - 1].isMine) boardModel[x][y - 1].mineCount++;
-        boardModel[x][y].isMine = true;
-        if (y < height - 1) if (!boardModel[x][y + 1].isMine) boardModel[x][y + 1].mineCount++;
-        if (x < width - 1 && y > 0) if (!boardModel[x + 1][y - 1].isMine) boardModel[x + 1][y - 1].mineCount++;
-        if (x < width - 1) if (!boardModel[x + 1][y].isMine) boardModel[x + 1][y].mineCount++;
-        if (x < width - 1 && y < height - 1) if (!boardModel[x + 1][y + 1].isMine) boardModel[x + 1][y + 1].mineCount++;
-    });
+    revealCell = (x, y) => {
+        // Ignore the reveal if the cell is already revealed
+        if (this.state.matrix[x][y].isRevealed) return;
 
-    return boardModel;
-}
+        this.setState({
+            matrix: this.state.matrix.map(column => column.map(cell => ({
+                ...cell,
+                isRevealed: cell.x === x && cell.y === y ? true : cell.isRevealed
+            })))
+        });
+    }
 
-function Board() {
-    return (
-        <div className='board'>
-            {createBoardModel(25, 25, 100).map(column =>
-                <div className='column' key={Math.random()}>
-                    {column.map(cell => <Cell key={Math.random()} isMine={cell.isMine} mineCount={cell.mineCount} />)}
-                </div>
-            )}
-        </div>
-    );
-}
+    toggleFlagCell = (x, y) => {
+        this.setState({
+            matrix: this.state.matrix.map(column => column.map(cell => ({
+                ...cell,
+                isFlagged: cell.x === x && cell.y === y ? !cell.isFlagged : cell.isFlagged
+            })))
+        });
+    }
 
-export default Board;
+    render = () => {
+        return (
+            <div className='board'>
+                {this.state.matrix.map(column =>
+                    <div className='column' key={Math.random()}>
+                        {column.map(cell => <Cell key={Math.random()} model={cell} revealCell={this.revealCell} toggleFlagCell={this.toggleFlagCell} />)}
+                    </div>
+                )}
+            </div>
+        );
+    }
+};
--- src/Cell/Cell.js
+++ src/Cell/Cell.js
@@ -2,33 +2,24 @@ import React from 'react';
 import './Cell.css';
 
 export default class Cell extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isRevealed: false,
-            isFlagged: false
-        };
-    }
-
     onClickHandler = event => {
         event.preventDefault();
 
-        if (event.type === 'contextmenu') {
-            this.setState({ isFlagged: !this.state.isFlagged });
-        } else if (event.type === 'click' && !this.state.isFlagged) {
-            this.setState({ isRevealed: true });
-        }
+        const { isFlagged, x, y } = this.props.model;
+        const { toggleFlagCell, revealCell } = this.props;
+
+        if (event.type === 'contextmenu') toggleFlagCell(x, y);
+        else if (event.type === 'click' && !isFlagged) revealCell(x, y);
     }
 
     render = () => {
-        const { isMine, mineCount } = this.props;
+        const { isMine, mineCount, isRevealed, isFlagged } = this.props.model;
 
         return (
             <React.Fragment>
-                {!this.state.isRevealed && <div className='cell' onClick={this.onClickHandler} onContextMenu={this.onClickHandler}>{this.state.isFlagged ? 'X' : ''}</div>}
-                {this.state.isRevealed && isMine && <div className='cell cell-mine'>M</div>}
-                {this.state.isRevealed && !isMine && <div className={`cell cell-value-${mineCount}`}>{mineCount > 0 ? mineCount : ''}</div>}
+                {!isRevealed && <div className='cell cell-hidden' onClick={this.onClickHandler} onContextMenu={this.onClickHandler}>{isFlagged ? 'X' : ''}</div>}
+                {isRevealed && isMine && <div className='cell cell-mine'>M</div>}
+                {isRevealed && !isMine && <div className={`cell cell-value-${mineCount}`}>{mineCount > 0 ? mineCount : ''}</div>}
             </React.Fragment>
         );
     }
